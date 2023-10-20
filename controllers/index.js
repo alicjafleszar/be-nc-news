f91e54cf69ce6c20021097a810cf6e49ef220b08
alicjafleszar <alicjafleszar@icloud.com>
2023-10-20 22:34:48 +0100

    create comments/comment_id endpoint - patch request

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const { getEndpointsInfo } = require("./base.controllers");
 const { getTopics } = require("./topics.controllers");
 const { getArticleById, getArticles, getCommentsByArticleId, postComment, patchArticle } = require("./articles.controllers");
-const { deleteCommentById } = require("./comments.controllers");
+const { deleteCommentById, patchCommentById } = require("./comments.controllers");
 const { getUsers, getUser } = require("./users.controllers")
 
-module.exports = { getEndpointsInfo, getTopics, getArticleById, getArticles, getCommentsByArticleId, postComment, patchArticle, deleteCommentById, getUsers, getUser };
+module.exports = { getEndpointsInfo, getTopics, getArticleById, getArticles, getCommentsByArticleId, postComment, patchArticle, deleteCommentById, getUsers, getUser, patchCommentById };
--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const { selectTopics } = require("./topics.models");
 const { selectArticleById, selectArticles, selectCommentsByArticleId, insertComment, updateArticle } = require("./articles.models");
-const { removeComment } = require("./comments.models");
+const { removeComment, updateComment } = require("./comments.models");
 const { selectUsers, selectUser } = require("./users.models");
 
-module.exports = { selectTopics, selectArticles, selectArticleById, selectCommentsByArticleId, insertComment, updateArticle, removeComment, selectUsers, selectUser };
+module.exports = { selectTopics, selectArticles, selectArticleById, selectCommentsByArticleId, insertComment, updateArticle, removeComment, selectUsers, selectUser, updateComment };
