874fac95ac532c5a68833b377b132c2310d04e69
alicjafleszar <alicjafleszar@icloud.com>
2023-10-21 00:53:12 +0100

    create delete article request endpoint

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const { getEndpointsInfo } = require("./base.controllers");
 const { getTopics } = require("./topics.controllers");
-const { getArticleById, getArticles, getCommentsByArticleId, postComment, patchArticle, postArticle } = require("./articles.controllers");
+const { getArticleById, getArticles, getCommentsByArticleId, postComment, patchArticle, postArticle, deleteArticleById } = require("./articles.controllers");
 const { deleteCommentById, patchCommentById } = require("./comments.controllers");
 const { getUsers, getUser } = require("./users.controllers")
 
-module.exports = { getEndpointsInfo, getTopics, getArticleById, getArticles, getCommentsByArticleId, postComment, patchArticle, deleteCommentById, getUsers, getUser, patchCommentById, postArticle };
+module.exports = { getEndpointsInfo, getTopics, getArticleById, getArticles, getCommentsByArticleId, postComment, patchArticle, deleteCommentById, getUsers, getUser, patchCommentById, postArticle, deleteArticleById };
--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const { selectTopics } = require("./topics.models");
-const { selectArticleById, selectArticles, selectCommentsByArticleId, insertComment, updateArticle, insertArticle } = require("./articles.models");
+const { selectArticleById, selectArticles, selectCommentsByArticleId, insertComment, updateArticle, insertArticle, removeArticle } = require("./articles.models");
 const { removeComment, updateComment } = require("./comments.models");
 const { selectUsers, selectUser } = require("./users.models");
 
-module.exports = { selectTopics, selectArticles, selectArticleById, selectCommentsByArticleId, insertComment, updateArticle, removeComment, selectUsers, selectUser, updateComment, insertArticle };
+module.exports = { selectTopics, selectArticles, selectArticleById, selectCommentsByArticleId, insertComment, updateArticle, removeComment, selectUsers, selectUser, updateComment, insertArticle, removeArticle };
